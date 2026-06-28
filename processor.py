457e8472e48b435246afa4e598b2c9741e58a06a
Rustam <rustamhidayatsimatupang10@gmai.com>
2026-06-28 19:52:45 +0700

    update processor.py

--- processor.py
+++ processor.py
@@ -1,10 +1,46 @@
-def ambil_data():
-    return ["Apel", "Jeruk", "Mangga", "Pisang"]
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime, timedelta
 
-def filter_data(data):
-    return [buah for buah in data if buah != "Pisang"]
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2026, 6, 28),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-data_mentah = ambil_data()
-data_bersih = filter_data(data_mentah)
-print(data_bersih)
-print("testing")
+with DAG(
+    'dag_isi_data_mart',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False
+) as dag:
+
+    # 1. Task untuk Membuat Tabel
+    create_table = PostgresOperator(
+        task_id='create_table_task',
+        postgres_conn_id='postgres_data_mart', # Sesuai dengan ID di Connections
+        sql="""
+        CREATE TABLE IF NOT EXISTS data_mart.penjualan_test (
+            id SERIAL PRIMARY KEY,
+            produk VARCHAR(100),
+            jumlah INT,
+            tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """
+    )
+
+    # 2. Task untuk Memasukkan Data
+    insert_data = PostgresOperator(
+        task_id='insert_data_task',
+        postgres_conn_id='postgres_data_mart',
+        sql="""
+        INSERT INTO data_mart.penjualan (produk, jumlah) VALUES 
+        ('Laptop', 1),
+        ('Mouse', 5),
+        ('Keyboard', 3);
+        """
+    )
+
+    create_table >> insert_data
